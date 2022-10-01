3984678ff1412ce39c044cf89eba81d846844a23
anvi-pen <91691176+anvi-pen@users.noreply.github.com>
2022-09-30 21:02:05 -0700

    Customer in progress

---
+++ Assets/Scripts/CustomerManagerScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerManagerScript : MonoBehaviour
+{
+    // private variables
+    [SerializeField] private GameObject customerPrefab;
+    private List<GameObject> currentCustomers;
+    private Queue<GameObject> futureCustomers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
