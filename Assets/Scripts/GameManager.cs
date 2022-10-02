0819151895b7ba853175343b7e8b0bd8f8bf0de2
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2022-10-01 17:35:10 -0700

    saving and audio mixer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,4 +28,45 @@ public class GameManager : MonoBehaviour
 
         fade.rectTransform.offsetMax = new Vector2(fade.rectTransform.offsetMax.x, -timer / 10 * 150);
     }
+
+    bool SaveSetting<T> (string s, T value)
+    {
+        if (typeof(T) == typeof(string))
+        {
+            PlayerPrefs.SetString(s, (string)(object)value);
+            return true;
+        }
+        if (typeof(T) == typeof(int))
+        {
+            PlayerPrefs.SetInt(s, (int)(object)value);
+            return true;
+        }
+        if (typeof(T) == typeof(float))
+        {
+            PlayerPrefs.SetFloat(s, (float)(object)value);
+            return true;
+        }
+
+        return false;
+    }
+
+    T LoadSetting<T>(string s)
+    {
+        if (PlayerPrefs.HasKey(s)) return default;
+        
+        if (typeof(T) == typeof(string))
+        {
+            return (T) Convert.ChangeType(PlayerPrefs.GetString(s), typeof(T));
+        }
+        if (typeof(T) == typeof(int))
+        {
+            return (T) Convert.ChangeType(PlayerPrefs.GetInt(s), typeof(T));
+        }
+        if (typeof(T) == typeof(float))
+        {
+            return (T) Convert.ChangeType(PlayerPrefs.GetFloat(s), typeof(T));
+        }
+
+        return default;
+    }
 }
