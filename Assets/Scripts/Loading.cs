5fb7aefc30de31438d0ca42f08c9c7f8455dec65
Faustine <faustinex.wang@gmail.com>
2022-10-02 13:38:25 -0700

    loading screen implemented

--- Assets/Scripts/Loading.cs
+++ Assets/Scripts/Loading.cs
@@ -1,18 +1,55 @@
 using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class Loading : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // canvas components to enable / disable
+    [SerializeField] private Canvas menu;
+    Canvas loading;
+
+    [SerializeField] Slider progressBar;
+    [SerializeField] TextMeshProUGUI loadingText;
+    [SerializeField] string sceneName = "SampleScene";
+
+    private void Start()
     {
+        loading = GetComponent<Canvas>();
         
+        // main menu enabled and loading disabled
+        loading.enabled = false;
+        menu.enabled = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
-        
+        StartCoroutine(LoadYourAsyncScene());
+        loading.enabled = true;
+        menu.enabled = false;
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            //MuncDuster grepper result
+            Debug.Log("Operation progress");
+
+            float progress = Mathf.Clamp01(asyncLoad.progress / 0.9f);
+
+            loadingText.text = Mathf.RoundToInt(progress * 100) + "%";
+            progressBar.value = progress;
+
+            yield return new WaitForEndOfFrame();
+
+        }
     }
 }
